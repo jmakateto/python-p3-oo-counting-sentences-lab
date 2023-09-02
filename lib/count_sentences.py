571514c109cabc933a0a183a6a1d1bf2540e8253
john makateto <jmakateto@gmail.com>
2023-09-02 18:17:20 +0300

    made changes

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,26 @@
 #!/usr/bin/env python3
 
+
 class MyString:
-  pass
+    def __init__(self, value=""):
+        if not isinstance(value, str):
+            raise ValueError("The value must be a string")
+        self.value = value
+
+    def is_sentence(self):
+        """Check if the value ends with a period."""
+        return self.value.endswith('.')
+
+    def is_question(self):
+        """Check if the value ends with a question mark."""
+        return self.value.endswith('?')
+
+    def is_exclamation(self):
+        """Check if the value ends with an exclamation mark."""
+        return self.value.endswith('!')
+
+    def count_sentences(self):
+        """Count the number of sentences in the value."""
+        sentence_endings = ['.', '?', '!']
+        sentences = [sentence.strip() for sentence in self.value.split() if sentence.endswith(tuple(sentence_endings))]
+        return len(sentences)
